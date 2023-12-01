c762f4499ded964a40e1294597b4ef544f03fee5
pawanevision <pchaurasia.evision@gmail.com>
2023-12-01 17:26:41 +0530

    commit 1

--- src/main/java/com/springBootTask/controller/Hello.java
+++ src/main/java/com/springBootTask/controller/Hello.java
@@ -1,4 +1,11 @@
 package com.springBootTask.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+
 }
