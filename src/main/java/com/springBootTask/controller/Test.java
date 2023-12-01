373af64d307275cf5bb4829f8f44e3b28cf5866f
pawanevision <pchaurasia.evision@gmail.com>
2023-12-01 17:29:17 +0530

    commit 3

--- src/main/java/com/springBootTask/controller/Test.java
+++ src/main/java/com/springBootTask/controller/Test.java
@@ -1,5 +1,8 @@
 package com.springBootTask.controller;
 
 public class Test {
-    
+    public static void main(String[] args) {
+        System.out.println("hello" +
+                "hi");
+    }
 }
